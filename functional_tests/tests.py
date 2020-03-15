4ce605317631f298389bb6ece842de0aac54df66
Jake Noble <jsmithnoble@gmail.com>
2020-03-14 21:11:51 -0600

    removed explicite sleep calls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -12,10 +15,17 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn('foo', [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -38,16 +48,15 @@ class NewVisitorTest(LiveServerTestCase):
         input_box.send_keys("Buy llama wool")
         # When she hits enter, the page updates. and now the page lists "1: But llama wool" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
         self.check_for_row_in_list_table("1: Buy llama wool")
         # There is still a text box inviting her to add another item.  She enters use llama wool to make a mitten.
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys("Use llama wool to make a mitten")
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
         # The page updates again and now shows both items on her lists
         self.check_for_row_in_list_table("1: Buy llama wool")
         self.check_for_row_in_list_table("2: Use llama wool to make a mitten")
+
         self.fail("Finish the test!")
         # Melanie wonders whether the site will remember her list.  Then she sees that the site has generated a unique url for her -- there is some explanatory text to that effect.
 
